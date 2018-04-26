8465477a2f1c66c76fe32aa013aa2b49e0622f72
Duncan McGreggor <oubiwann@gmail.com>
2018-04-25 21:17:17 -0500

    Added rudimentary day time transition support.

--- src/hxgm30/almanac/components/timer.clj
+++ src/hxgm30/almanac/components/timer.clj
@@ -1,15 +1,37 @@
 (ns hxgm30.almanac.components.timer
   (:require
+    [clojure.core.async :as async]
     [clojusc.twig :as logger]
     [com.stuartsierra.component :as component]
     [hxgm30.almanac.components.config :as config]
+    [hxgm30.almanac.const :as const]
+    [hxgm30.almanac.event.tag :as tag]
+    [hxgm30.event.components.pubsub :as pubsub]
     [taoensso.timbre :as log]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;   Utility Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; TBD
+(defn get-day-period
+  [system]
+  (/ const/day-seconds
+     (config/day-divisions system)
+     (config/time-multiplier system)))
+
+(defn create-timer
+  [system period event-type]
+  (async/go-loop []
+    (do
+      (pubsub/publish system :world event-type {})
+      (async/<! (async/timeout (* 1000 period)))
+      (recur))))
+
+(defn create-day-timer
+  [system]
+  (create-timer system
+                (get-day-period system)
+                tag/day-transition))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;   Timer Component API   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -27,13 +49,13 @@
   [this]
   (log/info "Starting timer component ...")
   (log/debug "Started timer component.")
-  this)
+  (assoc this :day (create-day-timer this)))
 
 (defn stop
   [this]
   (log/info "Stopping timer component ...")
   (log/debug "Stopped timer component.")
-  this)
+  (assoc this :day nil))
 
 (def lifecycle-behaviour
   {:start start
---
+++ src/hxgm30/almanac/const.clj
@@ -0,0 +1,6 @@
+(ns hxgm30.almanac.const)
+
+(def min-seconds 60)
+(def hour-seconds (* 60 min-seconds))
+(def day-seconds (* 24 hour-seconds))
+(def year-seconds (* 365.25 day-seconds))
